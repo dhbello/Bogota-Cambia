08649590a74203993e4593b36df5194ed31f7a10
David Bello <david.bello@sinesat.com>
2016-10-25 16:11:45 -0500

    Version 0.2

--- js/index.js
+++ js/index.js
@@ -2,15 +2,20 @@ var myApp = new Framework7();
 var $$ = Dom7;
 var map;
 var mapLayer;
+var glPoint;
+var currentPointX = -8244776.525;
+var currentPointY = 512845.78;
+var modeManual = false;
 
-myApp.addView('.view-main');
 gotoMain();
+myApp.addView('.view-main');
+
 
 if (isPhoneGapExclusive()) {
     document.addEventListener("deviceready", onDeviceReady, false);
 } else {
     $(document).ready(function () {
-        initMap();
+        initMap();        
     });
 };
 
@@ -22,7 +27,7 @@ function onDeviceReady() {
         }
     }
     $(document).ready(function () {
-        initMap();
+        initMap();        
     });
 }
 
@@ -46,10 +51,60 @@ function initMap2() {
         extent: new esri.geometry.Extent({ xmin: -8245377.08, ymin: 512468.58, xmax: -8244175.97, ymax: 513222.98, spatialReference: { wkid: 102100 } }),
         autoresize: false
     });
+    dojo.connect(map, "onClick", function (evt) {
+        if (modeManual) {
+            modeManual = false;
+            currentPointX = evt.mapPoint.x;
+            currentPointY = evt.mapPoint.y;
+            var currentPoint = new esri.geometry.Point(currentPointX, currentPointY, { wkid: 102100 });
+            glPoint.clear();
+            glPoint.add(new esri.Graphic(currentPoint,
+                    new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 15,
+                    new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color("#FF0000"), 2),
+                    new dojo.Color("#FF0000")),
+                    null, null));
+            map.centerAt(currentPoint);
+        }
+    });
     mapLayer = new esri.layers.ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer");
     map.addLayer(mapLayer);
+    glPoint = new esri.layers.GraphicsLayer();
+    glPoint.setRenderer(new esri.renderer.SimpleRenderer(
+            new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID,
+            new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
+            new dojo.Color([255, 0, 0, 0.75]), 2),
+            new dojo.Color([255, 0, 0, 0.75]))));
+    map.addLayer(glPoint, 0);
     updateSize();
+    initLocationGPS();
 }
+
+function initLocationGPS() {
+    try {
+ 
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pointWSG84 = esri.geometry.geographicToWebMercator(new esri.geometry.Point(position.coords.longitude, position.coords.latitude, { wkid: 3857 }));
+            currentPointX = pointWSG84.x;
+            currentPointY = pointWSG84.y;
+            var currentPoint = new esri.geometry.Point(currentPointX, currentPointY, { wkid: 102100 });
+            glPoint.clear();
+            glPoint.add(new esri.Graphic(currentPoint,
+                    new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 15,
+                    new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color("#FF0000"), 2),
+                    new dojo.Color("#FF0000")),
+                    null, null));
+            map.centerAt(currentPoint);
+        },
+            function (error) {
+                alert(error);
+            },
+            { timeout: 30000, enableHighAccuracy: true, maximumAge: 75000 });
+    } catch (err) {
+        alert(err);
+    }
+}
+
+
 
 function updateSize() {
     var the_height = window.innerHeight - $("#header").height() - $("#footer").height() - 10;
@@ -65,10 +120,16 @@ function updateSize() {
     };
 };
 
+function setLocation() {
+    modeManual = true;
+    glPoint.clear();
+};
+
 function gotoMain(){
     $("#mapDiv").css("left", "-2000px");
     $("#mapDiv").css("position", "absolute");
     $("#map-toolbar").hide();
+    $("#map-header").hide();
 
     $("#reporteDiv").hide();
     $("#reporte-toolbar").hide();
@@ -81,6 +142,7 @@ function gotoMap(){
      $("#mapDiv").css("left", "0px");
      $("#mapDiv").css("position", "");
      $("#map-toolbar").show();
+     $("#map-header").show();
 
      $("#bienvenidaDiv").hide();
      $("#bienvenida-toolbar").hide();
@@ -93,12 +155,25 @@ function gotoReporte() {
     $("#mapDiv").css("left", "-2000px");
     $("#mapDiv").css("position", "absolute");
     $("#map-toolbar").hide();
+    $("#map-header").hide();
 
     $("#bienvenidaDiv").hide();
     $("#bienvenida-toolbar").hide();
 
     $("#reporteDiv").show();
     $("#reporte-toolbar").show();
+};
+
+function addPhotos() {
+    $("#photolist").append("<img src='icon.png' />");
+};
+
+function clearPhotos() {
+    $("#photolist").html("");
+};
+
+function submitReport() {
+
 };
 
 function isPhoneGapExclusive() {
