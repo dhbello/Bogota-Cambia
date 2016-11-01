b121d5f00a9fff97ab8adda9a539add25c25a9aa
David Bello <david.bello@sinesat.com>
2016-11-01 16:47:20 -0500

    Version 0.37

--- js/index.js
+++ js/index.js
@@ -6,16 +6,23 @@ var $$ = Dom7;
 var map;
 var view;
 var mapLayer;
+var marker;
 var glPoint;
 var glPointG;
 var currentPoint;
+var currentUser;
+var imageCache;
 
 var modeManual = false;
+var initRegistro = true;
 var photoURLS = new Array();
-var msgtitle = "Dinamica Urbana";
+var msgtitle = "Bogot&aacute; Cambia";
+var cambioStr = "Cambio de Uso"
 var baseMapUrl = "http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/Mapa_Base/MapServer";
 var _url_photo = 'https://dinamica-147714.appspot.com/Imagen';
-var _url_msg = 'https://dinamica-147714.appspot.com/Registro?';
+var _url_msg = 'https://20161101t163928-dot-dinamica-147714.appspot.com/Registro?';
+var _url_user = 'https://20161101t163928-dot-dinamica-147714.appspot.com/UsuarioRegistro?';
+var _url_balance = 'https://20161101t163928-dot-dinamica-147714.appspot.com/UsuarioBalance?';
 
 gotoMain();
 
@@ -35,6 +42,7 @@ function onDeviceReady() {
 }
 
 function init() {
+    currentUser = window.localStorage.getItem("user");
     if (isPhoneGapExclusive()) {
         if ((navigator.connection.type == 0) || (navigator.connection.type == 'none')) {
             myApp.alert('Esta aplicación requiere conexión a internet.', msgtitle);
@@ -42,6 +50,13 @@ function init() {
         }
     }
     initMap();
+    $('#ftipo').on('change', function () {
+        if (this.value == cambioStr) {
+            $("#detalleTipo").show();
+        } else {
+            $("#detalleTipo").hide();
+        }
+    });
 }
 
 function initMap() {
@@ -50,7 +65,7 @@ function initMap() {
         dojo.require("esri.layers.MapImageLayer");
         dojo.require("esri.layers.MapImage");
         dojo.require("esri.graphic");
-        dojo.require("esri.geometry.webMercatorUtils");
+        dojo.require("esri.symbols.PictureMarkerSymbol");
         dojo.addOnLoad(initMap2);
     } catch (err) {
 
@@ -66,15 +81,14 @@ function initMap2() {
     dojo.connect(map, "onClick", function (evt) {
         setLocationPoint(evt);
     });
+    marker = new esri.symbol.PictureMarkerSymbol();
+    marker.setHeight(44);
+    marker.setWidth(28);
+    marker.setUrl("css/Location_Icon.png");
 
     mapLayer = new esri.layers.ArcGISTiledMapServiceLayer("http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/Mapa_Base/MapServer/");
     map.addLayer(mapLayer);
     glPoint = new esri.layers.GraphicsLayer();
-    glPoint.setRenderer(new esri.renderer.SimpleRenderer(
-            new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID,
-            new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-            new dojo.Color([255, 0, 0, 0.75]), 2),
-            new dojo.Color([255, 0, 0, 0.75]))));
     map.addLayer(glPoint, 0);
     updateSize();
     initLocationGPS();
@@ -88,11 +102,7 @@ function initLocationGPS() {
             currentPointY = position.coords.latitude;
             var currentPoint = new esri.geometry.Point(currentPointX, currentPointY, { wkid: 4686 });
             glPoint.clear();
-            glPoint.add(new esri.Graphic(currentPoint,
-                    new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 15,
-                    new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color("#FF0000"), 2),
-                    new dojo.Color("#FF0000")),
-                    null, null));
+            glPoint.add(new esri.Graphic(currentPoint, marker), null, null);
             map.centerAt(currentPoint);
         },
             function (error) {
@@ -105,7 +115,7 @@ function initLocationGPS() {
 }
 
 function updateSize() {
-    var the_height = window.innerHeight - $("#header").height() - $("#footer").height() - 10;
+    var the_height = window.innerHeight - $("#header").height() - $("#footer").height();
     $("#map").height(the_height);
     if (map) {
         map.resize();
@@ -125,71 +135,199 @@ function setLocationPoint(evt) {
         currentPointY = evt.mapPoint.y;
         var currentPoint = new esri.geometry.Point(currentPointX, currentPointY, { wkid: 4686 });
         glPoint.clear();
-        glPoint.add(new esri.Graphic(currentPoint,
-                new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 15,
-                new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color("#FF0000"), 2),
-                new dojo.Color("#FF0000")),
-                null, null));
+        glPoint.add(new esri.Graphic(currentPoint, marker), null, null);
         map.centerAt(currentPoint);
     }
 }
 
-function gotoMain() {
+function hideAll() {
     $("#mapDiv").css("left", "-2000px");
     $("#mapDiv").css("position", "absolute");
-    $("#map-toolbar").hide();
-    $("#map-header").hide();
+    $("#map-header1").hide();
+    $("#map-header2").hide();
+    $("#speed-dial").hide();
 
+    $("#footer").hide();
     $("#reporteDiv").hide();
     $("#reporte-toolbar").hide();
+    $("#bienvenidaDiv").hide();
+    $("#bienvenida-toolbar").hide();
+    $("#registroDiv").hide();
+    $("#registro-toolbar").hide();
+    $("#terminosDiv").hide();
+    $("#terminos-toolbar").hide();
+    $("#catalogoDiv").hide();
+    $("#catalogo-toolbar").hide();
+    $("#empty-header").hide();
+};
 
+function gotoNext() {
+    hideAll();
+    if (currentUser == null) {
+        gotoRegistro();
+    } else {
+        gotoMap();
+        updateUser();
+    };
+};
+
+function submitRegistro() {
+    if (initRegistro) {
+        $.validity.start();
+        $("#fcorreo").require();
+        if ($.validity.end().errors > 0) {
+            myApp.alert('Debe ingresar su direcci&oacute;n de correo.', msgtitle);
+            return;
+        };
+
+        $.validity.start();
+        $("#fcorreo").match("email");
+        if ($.validity.end().errors > 0) {
+            myApp.alert('Debe ingresar un correo electr&oacute;nico v&aacute;lido.', msgtitle);
+            return;
+        };
+
+        myApp.showPreloader("Validando usuario, por favor, espere.");
+        $.ajax({
+            url: _url_user + "email=" + encodeURIComponent($("#fcorreo").val().toLowerCase()),
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                myApp.hidePreloader();
+                if (response.status == "true") {
+                    currentUser = $("#fcorreo").val().toLowerCase();
+                    window.localStorage.setItem("user", currentUser);
+                    gotoMap();
+                    updateUser();
+                } else {
+                    initRegistro = false;
+                    $("#registroNuevo").show();
+                };
+            },
+            error: function () {
+                myApp.hidePreloader();
+                myApp.alert('No se pudo validar el usuario, por favor, intente m&aacute;s tarde.', msgtitle);
+            }
+        });
+    } else {
+        $.validity.start();
+        $("#fnombres").require();
+        if ($.validity.end().errors > 0) {
+            myApp.alert('Debe ingresar su nombre.', msgtitle);
+            return;
+        };
+        $.validity.start();
+        $("#fapellidos").require();
+        if ($.validity.end().errors > 0) {
+            myApp.alert('Debe ingresar su apellido.', msgtitle);
+            return;
+        };
+        if ($("#terminosCheck").prop('checked') == false) {
+            myApp.alert('Debe aceptar los terminos de uso.', msgtitle);
+            return;
+        }
+        $.ajax({
+            url: _url_user + "email=" + encodeURIComponent($("#fcorreo").val().toLowerCase()) +
+                "&nombres=" + encodeURIComponent($("#fnombres").val()) + "&apellidos=" + encodeURIComponent($("#fapellidos").val()),
+            type: 'POST',
+            dataType: 'json',
+            success: function (response) {
+                myApp.hidePreloader();
+                if (response.status == "true") {
+                    currentUser = $("#fcorreo").val().toLowerCase();
+                    window.localStorage.setItem("user", currentUser);
+                    gotoMap();
+                    updateUser();
+                } else {
+                    myApp.alert('No se pudo registrar el usuario, por favor, intente m&aacute;s tarde.', msgtitle);
+                };
+            },
+            error: function () {
+                myApp.hidePreloader();
+                myApp.alert('No se pudo registrar el usuario, por favor, intente m&aacute;s tarde.', msgtitle);
+            }
+        });
+    };
+};
+
+function gotoMain() {
+    hideAll();
     $("#bienvenidaDiv").show();
     $("#bienvenida-toolbar").show();
+    $("#footer").show();
+    $("#empty-header").show();
 }
 
 function gotoMap() {
+    hideAll();
     $("#mapDiv").css("left", "0px");
     $("#mapDiv").css("position", "");
-    $("#map-toolbar").show();
-    $("#map-header").show();
-
-    $("#bienvenidaDiv").hide();
-    $("#bienvenida-toolbar").hide();
-
-    $("#reporteDiv").hide();
-    $("#reporte-toolbar").hide();
-
+    $("#map-header1").show();
+    $("#map-header2").show();
+    $("#speed-dial").show();
     updateSize();
 };
 
 function gotoReporte() {
-    $("#mapDiv").css("left", "-2000px");
-    $("#mapDiv").css("position", "absolute");
-    $("#map-toolbar").hide();
-    $("#map-header").hide();
-
-    $("#bienvenidaDiv").hide();
-    $("#bienvenida-toolbar").hide();
-
+    hideAll();
     $("#reporteDiv").show();
     $("#reporte-toolbar").show();
+    $("#footer").show();
+    $("#empty-header").show();
 
     photoURLS = new Array();
-    $('#fnombre')[0].value = "";
-    $('#fcorreo')[0].value = "";
     $('#fdescripcion')[0].value = "";
     $('#photolist').html("");
+    $('#ftipo').val("");
+    $("#detalleTipo").hide();
+};
+
+function gotoRegistro() {
+    initRegistro = true;
+    hideAll();
+    $("#registroDiv").show();
+    $("#registro-toolbar").show();
+    $("#footer").show();
+    $("#empty-header").show();
+}
+
+function gotoTerminos() {
+    hideAll();
+    $("#terminosDiv").show();
+    $("#terminos-toolbar").show();
+    $("#footer").show();
+    $("#empty-header").show();
+}
+
+function gotoCatalogo() {
+    hideAll();
+    myApp.closePanel('right');
+    $("#catalogoDiv").show();
+    $("#catalogo-toolbar").show();
+    $("#footer").show();
+    $("#empty-header").show();
+}
+
+function dial() {
+    if ($("#speed-dial").hasClass('speed-dial-opened')) {
+        $('#speed-dial').removeClass('speed-dial-opened');
+    } else {
+        $('#speed-dial').addClass('speed-dial-opened');
+    }
 };
 
 function addPhotos(sourceType) {
+    if (photoURLS.length == 3) {
+        myApp.alert("Solo puede cargar hasta tres fotos por reporte.", msgtitle);
+        return;
+    };
+
     navigator.camera.getPicture(captureSuccess, captureFail, {
         destinationType: Camera.DestinationType.FILE_URI,
         sourceType: sourceType,
         encodingType: Camera.EncodingType.JPEG
     });
 };
-
-var imageCache;
 
 function captureSuccess(imageURI) {
     myApp.showPreloader("Cargando foto, por favor, espere.");
@@ -219,7 +357,7 @@ function uploadSuccessFT(response) {
 
 function uploadFail(error) {
     myApp.hidePreloader();
-    myApp.alert("No se pudo cargar la foto, por favor, intente m&aacute;s tarde." + JSON.stringify(error), null, msgtitle);
+    myApp.alert("No se pudo cargar la foto, por favor, intente m&aacute;s tarde.", null, msgtitle);
 };
 
 function clearPhotos() {
@@ -228,20 +366,16 @@ function clearPhotos() {
 };
 
 function submitReport() {
+    if ($("#ftipo").val() == null) {
+        myApp.alert('Debe seleccionar un tipo de reporte.', msgtitle);
+        return;
+    }
+
     $.validity.start();
-    $("#fnombre").require();
-    $("#fcorreo").require();
     $("#ftipo").require();
     $("#fdescripcion").require();
     if ($.validity.end().errors > 0) {
         myApp.alert('Debe completar todos los campos para enviar un reporte.', msgtitle);
-        return;
-    };
-
-    $.validity.start();
-    $("#fcorreo").match("email");
-    if ($.validity.end().errors > 0) {
-        myApp.alert('Debe ingresar un correo electr&oacute;nico v&aacute;lido.', msgtitle);
         return;
     };
 
@@ -253,8 +387,16 @@ function submitReport() {
             photoMSG = photoMSG + '&foto=' + encodeURIComponent(photoURLS[i]);
         }
     };
-    var msgURL = _url_msg + "nombre=" + $('#fnombre')[0].value + "&email=" + $('#fcorreo')[0].value
-                          + "&tipoRegistro=" + $('#ftipo')[0].value + "&descripcion=" + $('#fdescripcion')[0].value
+
+    var tipoText;
+    if ($('#ftipo')[0].value == cambioStr) {
+        tipoText = $('#ftipo')[0].value + " - " + $('#ftipodetalle')[0].value;
+    } else {
+        tipoText = $('#ftipo')[0].value;
+    }
+
+    var msgURL = _url_msg + "email=" + encodeURIComponent(currentUser)
+                          + "&tipoRegistro=" + tipoText + "&descripcion=" + encodeURIComponent($('#fdescripcion')[0].value)
                           + "&latitud=" + currentPointX + "&longitud=" + currentPointY + photoMSG;
     $.ajax({
         url: msgURL,
@@ -262,6 +404,7 @@ function submitReport() {
         success: function () {                
             myApp.hidePreloader();
             myApp.alert('Reporte enviado exitosamente.', msgtitle);
+            updateUser();
         },
         error: function () {
             myApp.hidePreloader();
@@ -270,6 +413,27 @@ function submitReport() {
     });
 
     gotoMap();
+};
+
+function updateUser() {
+    $("#user_email").html(currentUser);
+    $.ajax({
+        url: _url_balance + "email=" + encodeURIComponent(currentUser),
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            myApp.hidePreloader();
+            if (response.status == "true") {
+                $("#user_name").html(response.nombres + " " + response.apellidos);
+                $("#ptTotales").html(response.ptTotales);
+                $("#ptPosibles").html(response.ptPosibles);
+                $("#ptPendientes").html(response.ptPendientes);
+            };
+        },
+        error: function () {
+
+        }
+    });
 };
 
 function isPhoneGapExclusive() {
